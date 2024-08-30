82af8bfd623372008441fb9fae2bf3e56984a9b2
trungtin tran <119394773+3t-ttt@users.noreply.github.com>
2024-08-31 08:49:07 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.title("Hello World from CloudMentorPro")
-st.title(" test cicd code pipeline ")
+st.title(" deloy ecs blue/green ")
+# st.title(" test cicd code pipeline ")
